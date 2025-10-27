2f8bb06cec2f155c93f4e32f8af2d5e9dee76e30
shashikumar <kumar@shashi.com>
2025-10-27 19:10:10 +0530

    updated of foot wear and design

--- operation.js
+++ operation.js
@@ -1,4 +1,16 @@
+let cartCount=0;
+const cartCountElement = document.getElementById("cart-count");
+const addToCartButtons = document.getElementsByClassName("buynow");
 
-document.getElementsByClassName("buynow")
+for(let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click", function () {
+        cartCount++;
+        cartCountElement.textContent = cartCount;
+    });
+}
 
-console.log(8)
+//--------toogle button-----//
+const bgchange = document.querySelector(".toogle");
+bgchange.addEventListener("click",()=>{
+    document.body.classList.toggle("dark-mode");
+})
